c97152708bd0c34c27fdb57ac39509a1e8913a82
hhoangvan <hhoangvan@P5VN-PORT070>
2022-08-24 10:49:13 +0700

    Adding a new class library and update dencies of MVC5 to latest version

---
+++ AspNetMvc5.DemoLib/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetMvc5.DemoLib
+{
+    public class Utils
+    {
+        public static string GetDateTimeNow()
+        {
+            return "Server time now is " + DateTime.Now.ToString();
+        }
+    }
+}
